4fba430f76a9cbc4d7b9c9279b72f4a29f927aef
James Higgs <james.y.higgs@gmail.com>
2019-07-19 12:03:56 -0700

    pinClick function

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-
+//click event for submit
 function citySubmit() {
     $(".submit").click(function(event) {
         event.preventDefault();
@@ -10,31 +10,52 @@ function citySubmit() {
     });
 }
 
-function project() {
+//click event for pins
+//add listener
+function pinClick() { 
+google.maps.event.addListener(marker, "click", function(event) {
+    var latitude = event.latLng.lat();
+    var longitude = event.latLng.lng();
+    console.log( latitude + ', ' + longitude );
+}); //end addListener
+}
+
+/*function project() {
     $(".project").click(function(event) {
         location.href = 'https://jahpe777.github.io/Quiz_App/';
     });
-}
+}*/
+
 
 citySubmit();
+pinClick();
 /*project();*/
 
+
+//kelvin to fahrenheit
+const kelvin = prompt('What is the Kelvin temperature today?');
+const celsius = kelvin -273;
+
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
+
+
 function initMap() {
-    var uluru = {lat: cities[0].geometry.coordinates[1], lng: cities[0].geometry.coordinates[0]};
+    var coordinates = {lat: cities[0].geometry.coordinates[1], lng: cities[0].geometry.coordinates[0]};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {zoom: 4, center: coordinates});
     for (let city of cities) {
-        //console.log(city)
+        console.log(city)
         var marker = new google.maps.Marker({position: {
                                                 lat: city.geometry.coordinates[1], 
                                                 lng: city.geometry.coordinates[0]}, 
                                                 map: map
                                             });
     }
-  //var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker = new google.maps.Marker({position: coordinates, map: map});
   };
 
-  /*
+
   fetch(`https://app.climate.azavea.com/api/climate-data/${cities[0].geometry.coordinates[1]}/${cities[0].geometry.coordinates[0]}/RCP85?dataset=LOCA`, { headers: { Authorization: 'token 8428d0e3ca7a3f5862681ad13cb428d7e6f77a9d'}})  
     .then(function(response) {
         return response.json();
@@ -42,5 +63,3 @@ function initMap() {
       .then(function(myJson) {
         console.log(myJson);
       });
-
-  */
