ba907928d11e7b7df5a36f36aea4403284951238
James Higgs <james.y.higgs@gmail.com>
2019-07-21 13:27:10 -0700

    for fetch

--- assets/js/api.js
+++ assets/js/api.js
@@ -10,10 +10,8 @@ function citySubmit() {
     });
 }
 
-function latLngSubmit() {
-    google.maps.event.addListener(marker, "click", function() { 
-        alert("works");
-        event.preventDefault();
+function latLngSubmit(marker) {
+    google.maps.event.addListener(marker, "click", function(event) { 
         $(".tempModal").css("display", "flex");
     });
     $(".close").click(function(event) {
@@ -35,7 +33,7 @@ function pinClick(marker) {
 
 
 citySubmit();
-latLngSubmit();
+
 
 
 /*function displayResults(responseJson) {
@@ -45,7 +43,7 @@ latLngSubmit();
         <h3>${responseJson.data.tasmax}</h3>
       )};  */
 
-      
+
 /*//kelvin to fahrenheit
 const kelvin = prompt("What is the Kelvin temperature today?");
 const celsius = kelvin -273;
@@ -65,17 +63,49 @@ function initMap() {
                                                 lng: city.geometry.coordinates[0]}, 
                                                 map: map
                                             });
-        pinClick(marker); // pass each marker to the pin click function to set the listener                                    
+        pinClick(marker); // pass each marker to the pin click function to set the listener
+        latLngSubmit(marker);                                     
     }
     
   var marker = new google.maps.Marker({position: coordinates, map: map});
   };
 
 
-  fetch(`https://app.climate.azavea.com/api/climate-data/${cities[0].geometry.coordinates[1]}/${cities[0].geometry.coordinates[0]}/RCP85?dataset=LOCA`, { headers: { Authorization: "token 8428d0e3ca7a3f5862681ad13cb428d7e6f77a9d"}})  
+    fetch(`https://app.climate.azavea.com/api/climate-data/${cities[0].geometry.coordinates[1]}/${cities[0].geometry.coordinates[0]}/RCP85?dataset=LOCA`, { headers: { Authorization: "token 8428d0e3ca7a3f5862681ad13cb428d7e6f77a9d"}})  
     .then(function(response) {
         return response.json();
       })
       .then(function(myJson) {
         console.log(myJson);
       });
+
+    /*fetch(`https://app.climate.azavea.com/api/climate-data/${cities[0].geometry.coordinates[1]}/${cities[0].geometry.coordinates[0]}/RCP85?dataset=LOCA`, { headers: { Authorization: "token 8428d0e3ca7a3f5862681ad13cb428d7e6f77a9d"}}) 
+    .then(function(response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+      if (responseJson.items.length === 0){
+        alert(`We couldn't find you any animals. Try choosing something else`);
+      }
+      else {displayResults(responseJson);}
+    })
+    .catch(err => {
+      if (err.message == "Cannot read property 'id' of undefined"){
+        alert("We couldn't find any videos of that animal, try another!");
+      } else{
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      }
+    });
+}
+
+var request = require("request");
+
+console.log("The temperature is...");
+request(`https://app.climate.azavea.com/api/climate-data/${cities[0].geometry.coordinates[1]}/${cities[0].geometry.coordinates[0]}/RCP85?dataset=LOCA`, { headers: { Authorization: "token 8428d0e3ca7a3f5862681ad13cb428d7e6f77a9d"}})
+    if(!error && response.statuscode == 200) {
+        var parsedData = JSON.parse(body);
+        console.log(parsedData["data"]);
+    }*/
